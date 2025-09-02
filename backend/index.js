d6579cc0ddce49e51f5d5cab225e1dd5d039506d
¨LuchooLopez¨ <Luchoolopez2810@gmail.com>
2025-09-02 20:42:35 -0300

    anda mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
+require('dotenv').config({ path: '../.env'});
 const postRoutes = require('./routes/postRoutes');
 const userRoutes = require('./routes/userRoutes');
 
-app.use(express.json);
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('mongoDB conectado'))
+.catch(err => console.error('Error de conexion:', err));
 
 app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
@@ -12,5 +20,5 @@ app.use('/api/users', userRoutes);
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    `Servidor corriendo en el puerto ${PORT}`
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,7 +3,7 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-router.post('/register', userController.registerUser);
-router.post('/login', userController.loginUser);
+router.post('/register', userController.register);
+router.post('/login', userController.login);
 
 module.exports = router;
