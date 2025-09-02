2c5ec7ecf0ca1547cf34065c8070a47e305bd81a
¨LuchooLopez¨ <Luchoolopez2810@gmail.com>
2025-09-02 18:56:24 -0300

    configurando el package

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const postRoutes = require('./routes/postRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+app.use(express.json);
+
+app.use('/api/posts', postRoutes);
+app.use('/api/users', userRoutes);
+
+const PORT = 3000;
+
+app.listen(() => {
+    `Servidor corriendo en el puerto ${PORT}`
+});
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -4,13 +4,13 @@ const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-// Public routes
-router.get('/', postController.getAllPosts); // Get all posts
-router.get('/:id', postController.getPostById); // Get a single post by ID
+//publico
+router.get('/', postController.getAllPosts); 
+router.get('/:id', postController.getPostById); 
 
-// Protected routes
-router.post('/', authMiddleware.protect, postController.createPost); // Create a new post
-router.patch('/:id', authMiddleware.protect, postController.updatePost); // Update a post
-router.delete('/:id', authMiddleware.protect, postController.deletePost); // Delete a post
+//privado
+router.post('/', authMiddleware.protect, postController.createPost); 
+router.patch('/:id', authMiddleware.protect, postController.updatePost); 
+router.delete('/:id', authMiddleware.protect, postController.deletePost);
 
 module.exports = router;
